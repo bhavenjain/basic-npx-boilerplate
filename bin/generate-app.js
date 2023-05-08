edf7efae004e1dc78ec3cf86e19fab00dc69a155
bhaven360 <123927634+bhaven360@users.noreply.github.com>
2023-05-08 13:44:18 +0530

    changes

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import { execSync } from "child_process"
+// import { execSync } from "child_process"
 import path from "path"
 import fs from "fs"
-// const { execSync } = require('child_process');
+const { execSync } = require('child_process');
 // const fs = require('fs')
 // const path = require('path')
 
